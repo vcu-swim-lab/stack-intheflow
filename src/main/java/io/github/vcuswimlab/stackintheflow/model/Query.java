5a15dc39e292cfd39ee911a779848573999f8046
“Zerthick” <zerthick@gmail.com>
2016-10-27 14:35:16 -0400

    Refactored Query object to utilize one map.

--- src/main/java/io/github/vcuswimlab/stackintheflow/model/Query.java
+++ src/main/java/io/github/vcuswimlab/stackintheflow/model/Query.java
@@ -7,50 +7,71 @@ import java.util.Map;
  * Created by Jeet on 10/18/2016.
  */
 public class Query {
-    private Map<Component, String> components;
+    private Map<String, String> components;
 
     public enum Component {
-        Q,
-        TAGGED,
 
-        PAGE,
-        PAGESIZE,
-        FROMDATE,
-        TODATE,
-        MIN,
-        MAX,
-        ANSWERS,
-        BODY,
-        NOTTAGGED,
-        TITLE,
-        USER,
-        URL,
-        VIEWS,
+        Q("q"),
+        TAGGED("tagged"),
 
-        ORDER,
-        SORT,
-        ACCEPTED,
-        CLOSED,
-        MIGRATED,
-        NOTICE,
-        WIKI,
+        PAGE("page"),
+        PAGESIZE("pagesize"),
+        FROMDATE("fromdate"),
+        TODATE("todate"),
+        MIN("min"),
+        MAX("max"),
+        ANSWERS("answers"),
+        BODY("body"),
+        NOTTAGGED("nottagged"),
+        TITLE("title"),
+        USER("user"),
+        URL("url"),
+        VIEWS("views"),
+
+        ORDER("oder"),
+        SORT("sort"),
+        ACCEPTED("accepted"),
+        CLOSED("closed"),
+        MIGRATED("migrated"),
+        NOTICE("notice"),
+        WIKI("wiki");
+
+        private final String label;
+
+        Component(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
     }
 
     public Query(String q) {
         this(new HashMap<>(), q);
     }
 
-    public Query(Map<Component, String> components, String q) {
+    public Query(Map<String, String> components, String q) {
         this.components = components;
         set(Component.Q,q);
     }
 
     public Query set(Component component, String value) {
+        components.put(component.toString(), value);
+        return this;
+    }
+
+    public Query set(String component, String value) {
         components.put(component, value);
         return this;
     }
 
-    public String get(String name) {
-        return components.get(name);
+    public String get(Component component) {
+        return components.get(component.toString());
+    }
+
+    public String get(String compoment) {
+        return components.get(compoment);
     }
 }
