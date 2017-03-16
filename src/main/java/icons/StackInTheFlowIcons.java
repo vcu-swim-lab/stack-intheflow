2b87f359f65020e48c10ae5b06cc71f91f2c50d3
Zerthick <zerthick@gmail.com>
2017-03-16 14:59:26 -0400

    Implemented Icons in ToolWindow and AutoQuery Action.

---
+++ src/main/java/icons/StackInTheFlowIcons.java
@@ -0,0 +1,13 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by chase on 3/16/17.
+ */
+public interface StackInTheFlowIcons {
+    Icon ACTION_ICON = IconLoader.getIcon("/icons/actionIcon.png");
+    Icon TOOL_WINDOW_ICON = IconLoader.getIcon("/icons/toolWindowIcon.png");
+}
