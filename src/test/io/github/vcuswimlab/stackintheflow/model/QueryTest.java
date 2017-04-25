f9a8491cc5d007425a8ba919041572b6bd02cd01
Zerthick <zerthick@gmail.com>
2017-04-25 16:08:53 -0400

    Added initial tests, refactored plugin module dependencies in order to enable testing.

---
+++ src/test/io/github/vcuswimlab/stackintheflow/model/QueryTest.java
@@ -0,0 +1,49 @@
+package io.github.vcuswimlab.stackintheflow.model;
+
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
+import jersey.repackaged.com.google.common.collect.ImmutableSet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by chase on 4/25/17.
+ */
+public class QueryTest {
+
+    private Query query;
+
+    @Before
+    public void setUp() throws Exception {
+        query = new Query("mySite");
+    }
+
+    @Test
+    public void testSetComponent() throws Exception {
+        query.set(Query.Component.Q, "myQ");
+        assertEquals("myQ", query.get(Query.Component.Q));
+    }
+
+    @Test
+    public void testSetString() throws Exception {
+        query.set("q", "myQ");
+        assertEquals("myQ", query.get(Query.Component.Q));
+    }
+
+    @Test
+    public void get() throws Exception {
+        assertEquals("mySite", query.get(Query.Component.SITE));
+    }
+
+    @Test
+    public void getComponents() throws Exception {
+        assertEquals(ImmutableSet.of("site"), query.getComponents());
+    }
+
+    @Test
+    public void getComponentMap() throws Exception {
+        assertEquals(ImmutableMap.of("site", "mySite"), query.getComponentMap());
+    }
+
+}
---
+++ src/test/io/github/vcuswimlab/stackintheflow/model/QuestionTest.java
@@ -0,0 +1,111 @@
+package io.github.vcuswimlab.stackintheflow.model;
+
+import jersey.repackaged.com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chase on 4/25/17.
+ */
+public class QuestionTest {
+
+    private Question question;
+
+    @Before
+    public void setUp() throws Exception {
+        question = new Question(ImmutableList.of("Tag1", "Tag2"),
+                "myBody",
+                "myExcerpt",
+                "myTitle",
+                "myLink");
+    }
+
+    @Test
+    public void fixNulls() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTags() throws Exception {
+        assertEquals(ImmutableList.of("Tag1", "Tag2"), question.getTags());
+    }
+
+    @Test
+    public void testSetTags() throws Exception {
+        question.setTags(ImmutableList.of("Tag3", "Tag4"));
+        assertEquals(ImmutableList.of("Tag3", "Tag4"), question.getTags());
+    }
+
+    @Test
+    public void testGetBody() throws Exception {
+        assertEquals("myBody", question.getBody());
+    }
+
+    @Test
+    public void testSetBody() throws Exception {
+        question.setBody("myOtherBody");
+        assertEquals("myOtherBody", question.getBody());
+    }
+
+    @Test
+    public void testGetExcerpt() throws Exception {
+        assertEquals("myExcerpt", question.getExcerpt());
+    }
+
+    @Test
+    public void testSetExcerpt() throws Exception {
+        question.setExcerpt("myOtherExcerpt");
+        assertEquals("myOtherExcerpt", question.getExcerpt());
+    }
+
+    @Test
+    public void testGetTitle() throws Exception {
+        assertEquals("myTitle", question.getTitle());
+    }
+
+    @Test
+    public void testSetTitle() throws Exception {
+        question.setTitle("myOtherTitle");
+        assertEquals("myOtherTitle", question.getTitle());
+    }
+
+    @Test
+    public void testGetLink() throws Exception {
+        assertEquals("myLink", question.getLink());
+    }
+
+    @Test
+    public void testSetLink() throws Exception {
+        question.setLink("myOtherLink");
+        assertEquals("myOtherLink", question.getLink());
+    }
+
+    @Test
+    public void testIsExpanded() throws Exception {
+        assertFalse(question.isExpanded());
+    }
+
+    @Test
+    public void testToggleExpanded() throws Exception {
+        assertFalse(question.isExpanded());
+        question.toggleExpanded();
+        assertTrue(question.isExpanded());
+        question.toggleExpanded();
+        assertFalse(question.isExpanded());
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals("Question{tags=[Tag1, Tag2], body='myBody', excerpt='myExcerpt', " +
+                        "title='myTitle', link=myLink}",
+                question.toString());
+    }
+
+    @Test
+    public void testGetTagsAsFormattedString() throws Exception {
+        assertEquals("[Tag1] [Tag2] ", question.getTagsAsFormattedString());
+    }
+
+}
