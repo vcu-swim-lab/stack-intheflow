210256bcf89240c60983e7a80b49370e563a9cdb
Kevin <Kevin.ngo0407@gmail.com>
2017-07-05 13:45:20 -0400

    Implemented shrinking and expanding question results on click and a working excerpt. Wrote a lot of code to try to fix an anticipated problem where cutting off the string can cut it off in the middle of a tag. It turns out that HTML does not display a broken up tag, so this is not a problem. The code written is therefore unneccessary, but I am committing it anyways so that I have it somewhere in case it is useful later.

--- src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindow.js
+++ src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindow.js
@@ -2,15 +2,18 @@
 var questionsList;
 var numQuestions;
 var charCutoff;
+var questionDivs;
 
 $(document).ready(function(){
     questionsList = new Array();
+    questionDivs = new Array();
     numQuestions = 0;
     charCutoff = 300;
 });
 
 $("#searchButton").click(function(){
     JavaBridge.searchButtonClicked($('#searchBox').val());
+    generateListeners();
 });
 
 function Question(title, body, tags){
@@ -18,18 +21,39 @@ function Question(title, body, tags){
     this.body = body;
     this.tags = tags;
     this.codeTags = new Array();
+    this.htmlTags = new Array();
 
     this.findCodeTags = function(){
-        var lastStartIndex = 0;
-        var lastEndIndex = 0;
+        var lastStartIndex = -1;
+        var lastEndIndex = -1;
         while(true){
             var start = this.body.indexOf("<pre>", lastStartIndex + 1);
             var end = this.body.indexOf("</pre>", lastEndIndex + 1);
 
             if(start != end && start != -1){
-                this.codeTags.push(new Array(start, end));
+                this.codeTags.push(new CodeTag(start, end));
                 lastStartIndex = start;
                 lastEndIndex = end;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    this.findHTMLTags = function(){
+        var lastStartIndex = -1;
+        var lastEndIndex = -1;
+        while(true){
+            var startMatch = this.body.regexIndexOf("<\\w[^>]*>", lastStartIndex + 1);
+            var startIndex = startMatch.index;
+            var endMatch = this.body.regexIndexOf("<\\/[^>]+>", lastEndIndex + 1);
+            var endIndex = endMatch.index;
+
+            if(startIndex != endIndex && startIndex != -1){
+                this.htmlTags.push(new HTMLTag(startIndex, endIndex, endMatch.length));
+                lastStartIndex = startIndex;
+                lastEndIndex = endIndex;
             }
             else {
                 break;
@@ -42,47 +66,131 @@ function Question(title, body, tags){
     }
 
     this.lastCodeOpenTag = function(){
-        JavaBridge.print("In last code open method");
         if(this.hasCodeTags()){
-             JavaBridge.print("Has tags");
-             return this.codeTags[this.codeTags.length - 1][0];
+             return this.codeTags[this.codeTags.length - 1].open;
         }
         else {
-            JavaBridge.print("Doesn't have tags");
             return -1;
         }
     }
 
     this.lastCodeCloseTag = function(){
         if(this.hasCodeTags()){
-            return this.codeTags[this.codeTags.length - 1][1];
+            return this.codeTags[this.codeTags.length - 1].close;
         }
         else {
             return -1;
         }
     }
+
+    this.getCutoff = function(){
+        var tolerance = 100;
+        for(var i = this.codeTags.length - 1; i >= 0; i--){
+            if(Math.abs(this.codeTags[i].close - charCutoff) < tolerance){
+                return this.codeTags[i].close;
+            }
+            else if(Math.abs(this.codeTags[i].open - charCutoff) < tolerance){
+                return this.codeTags[i].open - 1;
+            }
+            else if(this.codeTags[i].open < charCutoff && this.codeTags[i].close > charCutoff){
+                return this.codeTags[i].open - 1;
+            }
+        }
+
+
+        for(var i = 0; i < this.htmlTags.length; i++){
+            if(charCutoff - this.htmlTags[i].open < this.htmlTags[i].length - 1 &&
+                charCutoff - this.htmlTags[i].open > 0){
+                JavaBridge.print("Special case - returning: " + this.htmlTags[i].open - 1);
+                return this.htmlTags[i].open - 1;
+            }
+            else if(charCutoff - this.htmlTags[i].close < this.htmlTags[i].length - 1 &&
+                charCutoff - this.htmlTags[i].close > 0){
+                JavaBridge.print("Special case - returning: " + this.htmlTags[i].close + this.htmlTags[i].length);
+                return this.htmlTags[i].close + this.htmlTags[i].length;
+            }
+        }
+
+        return charCutoff;
+    }
+
+    this.getShortenedContent = function(){
+        return this.body.substring(0, this.getCutoff());
+    }
+}
+
+function CodeTag(open, close){
+    this.open = open;
+    this.close = close;
+}
+
+function HTMLTag(open, close, length){
+    this.open = open;
+    this.close = close;
+    this.length = length;
 }
 
 function getQuestion(title, body, tags){
     questionsList.push(new Question(title, body, tags));;
+    questionsList[numQuestions].findCodeTags();
+    questionsList[numQuestions].findHTMLTags();
     numQuestions++;
 }
 
 function displayQuestions(){
     JavaBridge.print("Displaying questions!");
     for(var i = 0; i < numQuestions; i++){
-        JavaBridge.print("Question: " + i)
+        //JavaBridge.print("Question: " + i);
+        //JavaBridge.print(questionsList[i].body);
+        var questionDiv = $("<div>").addClass('contentShortened');
+
         var qTitle = $("<h3>").addClass('questionTitle').html(questionsList[i].title);
-        $('#questions').append(qTitle);
+        $(questionDiv).append(qTitle);
 
-        var qBody = $('<div>').addClass('questionBody').html(questionsList[i].body);
-        $('#questions').append(qBody);
+        var lastTag = charCutoff;
+        if(questionsList[i].hasCodeTags()){
+            lastTag = questionsList[i].lastCodeCloseTag();
+        }
 
-/*
-        for(var j = 0; j < questionsList[i].tags.length; j++){
-            $('#questions').append(questionsList[i].tags[j].toString());
-            $('#questions').append('<br />');
-        } */
+        var questionIndex = $('<span>').addClass('hidden').attr('id', 'questionIndex').html(i);
+
+        var qBody = $('<div>').addClass('questionBody').html(questionsList[i].getShortenedContent());
+        $(questionDiv).append(qBody);
+
+        $(questionDiv).append(questionIndex);
+
+        $('#questions').append(questionDiv);
+        questionDivs.push(questionDiv);
         $('#questions').append('<hr />');
     }
+}
+
+function generateListeners(){
+    for(var i = 0; i < questionDivs.length; i++){
+        $(questionDivs[i]).click(function(){
+            if($(this).hasClass('contentShortened')){
+                $(this).removeClass('contentShortened');
+                var index = $(this).find('#questionIndex').html();
+                $(this).find('.questionBody').html(questionsList[index].body);
+            }
+            else {
+                $(this).addClass('contentShortened');
+                var index = $(this).find('#questionIndex').html();
+                $(this).find('.questionBody').html(questionsList[index].getShortenedContent());
+            }
+        })
+    }
+}
+
+function RegexMatch(result, index, length){
+    this.result = result;
+    this.index = index;
+    this.length = length;
+}
+
+String.prototype.regexIndexOf = function(regex, fromIndex){
+  var str = fromIndex ? this.substring(fromIndex) : this;
+  var match = str.match(regex);
+
+  return new RegexMatch(match ? match[0] : "", match ? str.indexOf(match[0]) + fromIndex : -1, match ? match[0].length : -1);
 }
