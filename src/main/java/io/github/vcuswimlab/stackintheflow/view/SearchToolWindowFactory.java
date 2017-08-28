2590a460072aba50fc75d20d9a6b9d681df267bc
tylerjohnhaden <tylerjohnhaden@gmail.com>
2017-08-27 21:30:26 -0400

    Catch unfound JavaFX, display message in the tool window

--- src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
+++ src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
@@ -8,8 +8,16 @@ import io.github.vcuswimlab.stackintheflow.controller.component.stat.tags.UserTa
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SearchToolWindowFactory {
+    private static final String NO_JAVAFX_FOUND_MESSAGE =
+            "<html>" +
+                "Your platform does not support JavaFX." +
+                "<br />" +
+                "Please follow the <a href=\"\">instructions</a> to install the dependencies." +
+            "</html>";
+
     private JPanel content;
 
     public SearchToolWindowGUI buildGUI(@NotNull ToolWindow toolWindow, Project project) {
@@ -17,9 +25,24 @@ public class SearchToolWindowFactory {
                 .setContent(content)
                 .setProject(project)
                 .setSearchModel(project.getComponent(UserTagStatComponent.class).getSearchModel()).build();
+
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content windowContent = contentFactory.createContent(windowGUI.getContentPanel(), "", false);
+        Content windowContent;
+
+        // Check if javafx was found
+        if(windowGUI != null) {
+            windowContent = contentFactory.createContent(windowGUI.getContentPanel(), "", false);
+        } else {
+            windowContent = contentFactory.createContent(getNoJavaFXFoundPanel(), "", false);
+        }
+
         toolWindow.getContentManager().addContent(windowContent);
         return windowGUI;
+    }
+
+    private static JPanel getNoJavaFXFoundPanel() {
+        JPanel noJavaFXFoundPanel = new JPanel(new GridBagLayout());
+        noJavaFXFoundPanel.add(new JLabel(NO_JAVAFX_FOUND_MESSAGE));
+        return noJavaFXFoundPanel;
     }
 }
