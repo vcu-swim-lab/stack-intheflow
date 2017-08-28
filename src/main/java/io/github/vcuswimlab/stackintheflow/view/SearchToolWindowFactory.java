28cd4e94d44a58f538d48885598a15b5442b2798
tylerjohnhaden <tylerjohnhaden@gmail.com>
2017-08-27 22:19:56 -0400

    Enable hyperlink in the "no javafx found" panel

--- src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
+++ src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
@@ -1,5 +1,7 @@
 package io.github.vcuswimlab.stackintheflow.view;
 
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.ide.browsers.WebBrowserManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.content.Content;
@@ -8,6 +10,7 @@ import io.github.vcuswimlab.stackintheflow.controller.component.stat.tags.UserTa
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
 
 public class SearchToolWindowFactory {
@@ -15,7 +18,7 @@ public class SearchToolWindowFactory {
             "<html>" +
                 "Your platform does not support JavaFX." +
                 "<br />" +
-                "Please follow the <a href=\"\">instructions</a> to install the dependencies." +
+                "Please follow our <a href=\"http://github.com/vcu-swim-lab/stack-intheflow\">instructions</a> to install the dependencies." +
             "</html>";
 
     private JPanel content;
@@ -42,7 +45,17 @@ public class SearchToolWindowFactory {
 
     private static JPanel getNoJavaFXFoundPanel() {
         JPanel noJavaFXFoundPanel = new JPanel(new GridBagLayout());
-        noJavaFXFoundPanel.add(new JLabel(NO_JAVAFX_FOUND_MESSAGE));
+        JEditorPane noJavaFXFoundPane = new JEditorPane();
+        noJavaFXFoundPane.setContentType("text/html");
+        noJavaFXFoundPane.setEditable(false);
+        noJavaFXFoundPane.setOpaque(false);
+        noJavaFXFoundPane.setText(NO_JAVAFX_FOUND_MESSAGE);
+        noJavaFXFoundPane.addHyperlinkListener(e -> {
+            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                BrowserLauncher.getInstance().browse(e.getDescription(), WebBrowserManager.getInstance().getFirstActiveBrowser());
+            }
+        });
+        noJavaFXFoundPanel.add(noJavaFXFoundPane);
         return noJavaFXFoundPanel;
     }
 }
