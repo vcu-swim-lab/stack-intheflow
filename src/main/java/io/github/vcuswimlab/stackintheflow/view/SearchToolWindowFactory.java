9d7d755f54c2aa382e1525a03ef23afc17aa3f7f
Zerthick <zerthick@gmail.com>
2017-09-13 14:18:17 -0400

    Catch JavaFX

--- src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
+++ src/main/java/io/github/vcuswimlab/stackintheflow/view/SearchToolWindowFactory.java
@@ -1,5 +1,7 @@
 package io.github.vcuswimlab.stackintheflow.view;
 
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.ide.browsers.WebBrowserManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.content.Content;
@@ -8,18 +10,61 @@ import io.github.vcuswimlab.stackintheflow.controller.component.stat.tags.UserTa
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.*;
 
 public class SearchToolWindowFactory {
     private JPanel content;
 
+    private static final String NO_JAVAFX_FOUND_MESSAGE =
+            "<html>" +
+                    "Your platform does not support JavaFX." +
+                    "<br />" +
+                    "Please follow these <a href=\"http://github.com/vcu-swim-lab/stack-intheflow\">instructions</a> to install the dependencies." +
+            "</html>";
+
     public SearchToolWindowGUI buildGUI(@NotNull ToolWindow toolWindow, Project project) {
-        SearchToolWindowGUI windowGUI = new SearchToolWindowGUI.SearchToolWindowGUIBuilder()
-                .setContent(content)
-                .setProject(project)
-                .setSearchModel(project.getComponent(UserTagStatComponent.class).getSearchModel()).build();
+
+        SearchToolWindowGUI windowGUI = null;
+        JPanel jPanel;
+        if(isJavaFXAvailable()) {
+            windowGUI = new SearchToolWindowGUI.SearchToolWindowGUIBuilder()
+                    .setContent(content)
+                    .setProject(project)
+                    .setSearchModel(project.getComponent(UserTagStatComponent.class).getSearchModel()).build();
+            jPanel = windowGUI.getContentPanel();
+        } else {
+            jPanel = getNoJavaFXFoundPanel();
+        }
+
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content windowContent = contentFactory.createContent(windowGUI.getContentPanel(), "", false);
+        Content windowContent = contentFactory.createContent(jPanel, "", false);
         toolWindow.getContentManager().addContent(windowContent);
+
         return windowGUI;
+    }
+
+    private static JPanel getNoJavaFXFoundPanel() {
+        JPanel noJavaFXFoundPanel = new JPanel(new GridBagLayout());
+        JEditorPane noJavaFXFoundPane = new JEditorPane();
+        noJavaFXFoundPane.setContentType("text/html");
+        noJavaFXFoundPane.setEditable(false);
+        noJavaFXFoundPane.setOpaque(false);
+        noJavaFXFoundPane.setText(NO_JAVAFX_FOUND_MESSAGE);
+        noJavaFXFoundPane.addHyperlinkListener(e -> {
+            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                BrowserLauncher.getInstance().browse(e.getDescription(), WebBrowserManager.getInstance().getFirstActiveBrowser());
+            }
+        });
+        noJavaFXFoundPanel.add(noJavaFXFoundPane);
+        return noJavaFXFoundPanel;
+    }
+
+    private static boolean isJavaFXAvailable() {
+        try {
+            return Class.forName("javafx.scene.web.WebView") != null;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 }
